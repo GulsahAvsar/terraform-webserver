9e81ec9168dcbc562de37829c937f980100296f6
GulsahAvsar <avsa0005@algonquinlive.com>
2025-02-07 21:28:47 -0500

    Add resource definitions: RG, Public IP, VNet, Subnet, NSG, NIC, cloud-init, and VM

--- main.tf
+++ main.tf
@@ -26,7 +26,8 @@ provider "cloudinit" {
 }
 
 variable "labelPrefix" {
-  description = "Prefix for naming Azure resources"
+  type        = string
+  description = "College username. This will form the beginning of various resource names."
 }
 
 variable "region" {
@@ -35,7 +36,142 @@ variable "region" {
 }
 
 variable "admin_username" {
+  type        = string
   description = "The admin username for the virtual machine"
   default     = "azureadmin"
 }
+
+resource "azurerm_resource_group" "rg" {
+  name     = "${var.labelPrefix}-A05-RG"
+  location = var.region
+}
+
+resource "azurerm_public_ip" "web_ip" {
+  name                = "${var.labelPrefix}-A05-PublicIP"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Dynamic"
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "${var.labelPrefix}-A05-vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                 = "${var.labelPrefix}-A05-subnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+resource "azurerm_network_security_group" "nsg" {
+  name                = "${var.labelPrefix}-A05-nsg"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  security_rule {
+    name                       = "SSH"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "HTTP"
+    priority                   = 1002
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+resource "azurerm_network_interface" "nic" {
+  name                = "${var.labelPrefix}-A05-nic"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "${var.labelPrefix}A05-nic-config"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.web_ip.id
+  }
+
+  resource "azurerm_network_interface_security_group_association" "nsg" {
+  network_interface_id      = azurerm_network_interface.nsg.id
+  network_security_group_id = azurerm_network_security_group.nsg.id
+}
+
+# Define the init script template
+data "cloudinit_config" "init" {
+  gzip          = false
+  base64_encode = true
+
+  part {
+    filename     = "init.sh"
+    content_type = "text/x-shellscript"
+
+    content = file("${path.module}/init.sh")
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "vm" {
+  name                = "${var.labelPrefix}-A05-vm"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  size                = "Standard_B1s"
+  admin_username      = var.admin_username
+
+  network_interface_ids = [
+    azurerm_network_interface.nic.id,
+  ]
+
+  admin_ssh_key {
+    username   = var.admin_username
+    public_key = file("~/.ssh/id_rsa.pub") 
+  }
+
+  computer_name                   = "${var.labelPrefix}A05VM"
+  disable_password_authentication = true
+
+  os_disk {
+    name                 = "${var.labelPrefix}A05OSDisk"
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts-gen2"
+    version   = "latest"
+  }
+
+  custom_data = data.cloudinit_config.init.rendered
+}
+
+output "resource_group_name" {
+  description = "The name of the Azure Resource Group"
+  value       = azurerm_resource_group.rg.name
+}
+
+output "public_ip_address" {
+  description = "The public IP address of the web server"
+  value       = azurerm_public_ip.web_ip.ip_address
+}
+
+  
+
 
